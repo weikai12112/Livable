1434ee0f2bdccc5479a8adc57373e7d8b6f9cb5b
xh471087639 <471087639@qq.com>
2019-07-05 13:28:38 +0800

    add feedback modal

---
+++ js/currency.js
@@ -0,0 +1,27 @@
+$(document).ready(function () {
+    $('#FeedBack').click(function () {
+        $('#FeedBackBody').modal('show');
+    })
+    $('#upFeedBack').click(function () {
+        $.ajax({
+            type: 'post',
+            url: URL + '/getInformation',
+            contentType: 'application/x-www-form-urlencoded',
+            dataType: 'json',
+            async: true,
+            data: {
+                FeedBack:document.getElementById('#FeedBackInner').value,
+            },
+            success: function (result) {
+                if(result.code=='200'){
+                    document.getElementById('Name').innerHTML=result.data.Name;
+                    alert('已经收到您的意见啦~感谢您的反馈')
+                }
+                else alert(result.msg);
+            },
+            error: function () {
+                alert('服务器开小差啦');
+            }
+        })
+    })
+})
